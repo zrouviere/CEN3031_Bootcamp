dfd5ade0b9b9898a993c4676ca807440a76d937f
zrouviere <zrouviere@gmail.com>
2019-09-11 22:50:21 -0400

    add config

---
+++ config.js
@@ -0,0 +1,9 @@
+//This file holds any configuration variables we may need 
+//'config.js' is ignored by git to protect sensitive information, such as your database's username and password
+//copy this file's contents to another file 'config.js' and store your MongoLab uri there
+
+module.exports = {
+  db: {
+    uri: 'mongodb+srv://projUser:GniY5c0CKTBoNPBw@cen3031-mtusn.mongodb.net/test?retryWrites=true&w=majority', //place the URI of your mongo database here.
+  }
+};
